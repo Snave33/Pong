f1c5b3393ee7bb040f141281d788ff5b9b7a1d54
Snave333 <kaisorenson1@gmail.com>
2021-12-24 22:37:24 -0800

    add paddle a

--- pong.py
+++ pong.py
@@ -10,3 +10,16 @@ wn.tracer(0)
 #Main Game Loop
 while True:
     wn.update()
+
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("blue")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+#Paddle B
+
+
+#Ball
