afd94d891deef7861c414e44164b7f0157095dff
Snave333 <kaisorenson1@gmail.com>
2021-12-25 19:13:39 -0800

    Add Bounce off Paddle Mechanism

--- pong.py
+++ pong.py
@@ -33,8 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = .1
-ball.dy = .1
+ball.dx = .06
+ball.dy = .06
 
 #Functions
 
@@ -91,3 +91,14 @@ while True:
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+
+
+    #Paddle checking
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+
