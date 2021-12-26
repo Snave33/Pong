088514c587e7f0e68074c0aedc783f1a61c48510
Snave333 <kaisorenson1@gmail.com>
2021-12-25 18:19:03 -0800

    Added Ball and paddle b controls

--- pong.py
+++ pong.py
@@ -27,23 +27,43 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 #Ball
-paddle_b= turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("white")
-paddle_b.penup()
-paddle_b.goto(0, 0)
+ball= turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
-#Function
+#Functions
+
+#Paddle A Controls
 def paddle_a_up():
-    y = paddle_a.ycorr()
+    y = paddle_a.ycor()
     y += 20
     paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+#Paddle B Controls
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
 
 #Keyboard Binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
-
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 #Main Game Loop
 while True:
